289b78d254e00e96363ff4bc07297088178e45c3
Romain Fontaine <romain.fontaine@isen-lille.fr>
2017-02-22 11:47:49 +0100

    ajout comment

--- app/src/main/java/com/example/romain/intelikeyapp/MainActivity.java
+++ app/src/main/java/com/example/romain/intelikeyapp/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // action quand on selectionne un element du menu
         int id = item.getItemId();
 
         if (id == R.id.nav_led) {
